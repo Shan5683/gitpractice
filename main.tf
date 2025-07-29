2245d2088d31599a60a93d00d1b54983f14fd877
Your Name <youremail@example.com>
2025-07-29 15:02:45 -0500

    4 rg indore added

--- main.tf
+++ main.tf
@@ -12,3 +12,22 @@ provider "azurerm" {
     subscription_id = "5e1a4ed3-e4dc-4f98-88ec-6e69c7cf0255"
 }
 
+resource "azurerm_resource_group" "indore" {
+  name     = "indore1"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "indore2" {
+  name     = "indore2"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "indore3" {
+  name     = "indore3"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "indore4" {
+  name     = "indore4"
+  location = "West Europe"
+}
