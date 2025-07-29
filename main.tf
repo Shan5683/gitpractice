fb5e30439199d1c412786c8ecd2290084c0397d8
Your Name <youremail@example.com>
2025-07-29 14:48:14 -0500

    provider info added

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "5e1a4ed3-e4dc-4f98-88ec-6e69c7cf0255"
+}
+
